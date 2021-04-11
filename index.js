122279df141eb2777fa84d4d21d1ecec50b19157
Crepiks <sayazhan.onlassyn@mail.ru>
2021-04-11 15:17:50 +0600

    refactor: divide code into smaller functions

--- index.js
+++ index.js
@@ -5,31 +5,106 @@ const nodeGeocoder = require("node-geocoder");
 const main = () => {
   const arguments = process.argv.slice(2);
   const [firstArg, secondArg, thirdArg, fourthArg, fifthArg] = arguments;
-
   if (firstArg === "-w" && thirdArg === "-h") {
-    resizeImage(secondArg, fourthArg, fifthArg);
-  } else if (firstArg === "-c" || firstArg === "--city") {
-    displayCity(secondArg);
+    handleImageResize(secondArg, fourthArg, fifthArg);
+  } else if (firstArg === "-с" || firstArg === "--city") {
+    handleLogCity(secondArg);
   } else {
-    displayMetaData(firstArg);
+    handleLogMetaData(firstArg);
   }
 };
 
-const displayMetaData = (imgPath) => {
-  const srcPath = path.join(__dirname, imgPath);
-  imagemagick.readMetadata(srcPath, (error, metaData) => {
-    if (error) throw error;
-    const latitudeValues = metaData.exif.gpsLatitude.split(",");
-    const longitudeValues = metaData.exif.gpsLongitude.split(",");
-    const latitude = calculateGpsValues(latitudeValues);
-    const longitude = calculateGpsValues(longitudeValues);
+const handleImageResize = async (width, height, imagePath) => {
+  const source = path.join(__dirname, imagePath);
+  const [name, extension] = source.split(".");
+  const resizedImageName = `${name}_resized`;
+  const outputImagePath = path.resolve(`${resizedImageName}.${extension}`);
 
-    console.log(`lat: ${latitude}`);
-    console.log(`lon: ${longitude}`);
+  const outputFilePath = await resizeImage(
+    source,
+    outputImagePath,
+    width,
+    height
+  );
+  const outputFileSize = await getImageSize(outputFilePath);
+  
+  console.log(outputFileSize);
+};
+
+const resizeImage = (source, output, width, height) => {
+  return new Promise((resolve, reject) => {
+    imagemagick.resize(
+      { srcPath: source, dstPath: output, width, height },
+      (error) => {
+        if (error) reject(error);
+        resolve(output);
+      }
+    );
   });
 };
 
-const calculateGpsValues = (values) => {
+const getImageSize = (source) => {
+  return new Promise((resolve, reject) => {
+    imagemagick.identify(source, (error, info) => {
+      if (error) reject(error);
+      resolve(info.filesize);
+    });
+  });
+};
+
+const handleLogCity = async (imagePath) => {
+  const source = path.join(__dirname, imagePath);
+  const { lat, lon } = await getLocationMetaData(source);
+  const city = await getCityNameByLocation(lat, lon);
+
+  console.log(city);
+};
+
+const getCityNameByLocation = (lat, lon) => {
+  const options = {
+    provider: "yandex",
+    httpAdapter: "https",
+    apiKey: "9e282b5f-f010-4230-a264-56d192576794",
+    formatter: "json",
+  };
+  const geocoder = nodeGeocoder(options);
+
+  return new Promise((resolve, reject) => {
+    geocoder.reverse({ lat: `${lat}`, lon: `${lon}` }, (error, res) => {
+      if (error) reject(error);
+
+      const cityName = res[0].city;
+      resolve(cityName);
+    });
+  });
+};
+
+const handleLogMetaData = async (imagePath) => {
+  const source = path.join(__dirname, imagePath);
+  const { lat, lon } = await getLocationMetaData(source);
+
+  console.log(`lat: ${lat}`);
+  console.log(`lon: ${lon}`);
+};
+
+const getLocationMetaData = (source) => {
+  return new Promise((resolve, reject) => {
+    imagemagick.readMetadata(source, (error, meta) => {
+      if (error) {
+        reject(error);
+      }
+
+      const lat = meta.exif.gpsLatitude.split(",");
+      const lon = meta.exif.gpsLongitude.split(",");
+      const parsedLat = parseGPSLocation(lat);
+      const parsedLon = parseGPSLocation(lon);
+
+      resolve({ lat: parsedLat, lon: parsedLon });
+    });
+  });
+};
+
+const parseGPSLocation = (values) => {
   const parsedValues = values.map((value) => {
     const [numerator, denominator] = value.split("/");
     return parseInt(numerator) / parseInt(denominator);
@@ -38,49 +113,6 @@ const calculateGpsValues = (values) => {
   const value = degrees + minutes / 60 + seconds / 3600;
 
   return value;
-};
-
-const resizeImage = (width, height, imgPath) => {
-  const srcPath = path.resolve(__dirname + imgPath);
-  const [filePath, fileExtencion] = srcPath.split(".");
-  const redizedFilePath = `${filePath}_resized`;
-  const dstPath = path.resolve(`${redizedFilePath}.${fileExtencion}`);
-
-  imagemagick.resize({ srcPath, dstPath, width, height }, (err) => {
-    if (err) throw err;
-    imagemagick.identify(dstPath, (error, info) => {
-      if (error) throw error;
-      console.log(info.filesize);
-    });
-  });
-};
-
-const displayCity = (imgPath) => {
-  const options = {
-    provider: "yandex",
-    httpAdapter: "https",
-    apiKey: "9e282b5f-f010-4230-a264-56d192576794",
-    formatter: "json",
-  };
-  const geocoder = nodeGeocoder(options);
-  const srcPath = path.resolve(__dirname + imgPath);
-
-  imagemagick.readMetadata(srcPath, (error, metaData) => {
-    if (error) throw error;
-
-    const latitudeValues = metaData.exif.gpsLatitude.split(",");
-    const longitudeValues = metaData.exif.gpsLongitude.split(",");
-    const latitude = calculateGpsValues(latitudeValues);
-    const longitude = calculateGpsValues(longitudeValues);
-
-    geocoder.reverse(
-      { lat: `${latitude}`, lon: `${longitude}` },
-      (err, res) => {
-        if (err) throw error;
-        console.log(res[0].city);
-      }
-    );
-  });
 };
 
 main();
