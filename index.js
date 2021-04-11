ae4aafef6297ef5eb3c26b1dd313fbc7ce66dee7
Sultannis <sultan.fb29@gmail.com>
2021-04-11 11:36:49 +0600

    feat: finish application functionality

--- index.js
+++ index.js
@@ -4,41 +4,48 @@ const nodeGeocoder = require("node-geocoder");
 
 const main = () => {
   const arguments = process.argv.slice(2);
+  const [ firstArg, secondArg, thirdArg, fourthArg, fifthArg ] = arguments
 
-  if(arguments[0] === '-w' && arguments[2] === '-h') {
-    size(arguments[1], arguments[3], arguments[4])
+  if(firstArg === '-w' && thirdArg === '-h') {
+    resizeImage(secondArg, fourthArg, fifthArg)
   }
-
-  else if(arguments[0] === '-c' || arguments[0] === '--city') {
-    city(arguments[1])
+  else if(firstArg === '-c' || firstArg === '--city') {
+    displayCity(secondArg)
   }
-
   else {
-    const srcPath = path.resolve(__dirname + arguments[0]);
-      imagemagick.readMetadata(srcPath, (error, metaData) => {
-        let latitude = '';
-        let longitude = '';
-        if (error) throw error;
-        const latitudeValues = metaData.exif.gpsLatitude.split(',');
-        const longitudeValues = metaData.exif.gpsLongitude.split(',');
-    
-        latitude = calculateGpsValues(latitudeValues)
-        longitude = calculateGpsValues(longitudeValues)
-        
-        console.log(`lat: ${latitude} lon: ${longitude}`)
-      });
+    displayMetaData(firstArg)
   }
 };
 
+const displayMetaData = (imgPath) => {
+  const srcPath = path.resolve(__dirname + imgPath);
+  imagemagick.readMetadata(srcPath, (error, metaData) => {
+    if (error) throw error;
+    const latitudeValues = metaData.exif.gpsLatitude.split(',');
+    const longitudeValues = metaData.exif.gpsLongitude.split(',');
+    const latitude = calculateGpsValues(latitudeValues)
+    const longitude = calculateGpsValues(longitudeValues)
+
+    console.log(`lat: ${latitude}\nlon: ${longitude}`)
+  });
+}
+
 const calculateGpsValues = (values) => {
-  const value = parseFloat(values[0]) + parseFloat(values[1])/60 + parseFloat(values[2])/3600
+  const parsedValues = values.map(value => {
+    const [ numerator, denominator ] = value.split('/')
+    return parseInt(numerator)/parseInt(denominator)
+  })
+  const [degrees, minutes, seconds] = parsedValues
+  const value = degrees + minutes/60 + seconds/3600
 
   return value
 }
 
-const size = (width, height, imgPath) => {
+const resizeImage = (width, height, imgPath) => {
   const srcPath = path.resolve(__dirname + imgPath);
-  const dstPath = path.resolve(__dirname + '/data/IMG_0603_resized.HEIC');
+  const [filePath, fileExtencion] = srcPath.split('.')
+  const redizedFilePath = `${filePath}_resized`
+  const dstPath = path.resolve(`${redizedFilePath}.${fileExtencion}`);
 
   imagemagick.resize({ srcPath, dstPath, width, height }, (err) => {
     if (err) throw err;
@@ -49,36 +56,32 @@ const size = (width, height, imgPath) => {
   });
 };
 
-const city = (imgPath) => {
+const displayCity = (imgPath) => {
   const options = {
     provider: "yandex",
-    httpAdapter: "https", // Default
-    apiKey: "9e282b5f-f010-4230-a264-56d192576794", // for Mapquest, OpenCage, Google Premier
-    formatter: "json", // 'gpx', 'string', ...
+    httpAdapter: "https",
+    apiKey: "9e282b5f-f010-4230-a264-56d192576794",
+    formatter: "json",
   };
-
   const geocoder = nodeGeocoder(options);
-
   const srcPath = path.resolve(__dirname + imgPath);
 
   imagemagick.readMetadata(srcPath,  (error, metaData) => {
-    let latitude = '';
-    let longitude = '';
     if (error) throw error;
+  
     const latitudeValues = metaData.exif.gpsLatitude.split(',');
     const longitudeValues = metaData.exif.gpsLongitude.split(',');
+    const latitude = calculateGpsValues(latitudeValues)
+    const longitude = calculateGpsValues(longitudeValues)
 
-    latitude = calculateGpsValues(latitudeValues)
-    longitude = calculateGpsValues(longitudeValues)
-    
     geocoder.reverse(
       { lat: `${latitude}`, lon: `${longitude}` },
       (err, res) => {
-        console.log(res[0].formattedAddress)
+        if (err) throw error;
+        console.log(res[0].city)
       }
     );
   });
-
 };
 
 main();
